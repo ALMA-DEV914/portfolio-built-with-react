7fc829081170641767931162a430e828ef725abf
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-30 14:19:23 -0800

    add buttons for the link projects

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,23 +1,30 @@
 import React from "react";
+import computerImage from '../../assets/cover/computer.jpg';
+import githubImage from '../../assets/cover/github.jpeg';
 
 function ProjectList(props) {
   const { projects = [] } = props;
   return (
     <main>
+    
     <div className="projects">
       <ul className="flex-row">
         {projects.map((project) => (
           <li key={project.title}>
             <h3>{project.title}</h3>
-            <p><a href={project.deployment} target="_blank" rel="noreferrer">
-              {" "}
-             Deployed application
+            <div className="flex-row" id="icons">
+            <p className="btn"><a href={project.deployment} target="_blank" rel="noreferrer">
+              {" "}<span>View the App</span>
+              <img src={computerImage} className='my-2' style={{width: '40px'}} alt='cover'/> 
+            </a>
+            </p>
+            <p className="btn">
+            <a href={project.repository} target="_blank" rel="noreferrer">
+              {" "}<span>Source code</span>
+              <img src={githubImage} className='my-2' style={{width: '40px'}} alt='cover'/>
             </a></p>
-            <p><a href={project.repository} target="_blank" rel="noreferrer">
-              {" "}
-              Github repository
-            </a></p>
-            <img alt="projects" className="img-thumbnail mx-2"
+            </div>
+             <img alt="projects" className="img-thumbnail mx-2"
               src={require(`../../assets/${project.image}`)}
             ></img>
           </li>
