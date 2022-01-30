e669fe6e603dce7013f3a6fae42f46211898f520
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-29 23:08:05 -0800

    add icons for the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,25 @@
 import React from "react";
+import {Linkedin, Github, StackOverflow} from 'react-bootstrap-icons';
 
 
-function Footer(){
+export default function Footer(){
   return (
       <footer className="footer">
       <div>
-        <a href="https://github.com/alma-dev914" target="_blank" rel="noreferrer">
+        <a href="https://github.com/alma-dev914" target="_blank" rel="noreferrer"> <Linkedin />
         </a>
-      </div>
-      <div>
+
         <a href="https://www.linkedin.com/in/alma-braun-1b11731aa/" target="_blank" rel="noreferrer">
-          
+          <Github />
         </a>
-      </div>
-      <div>
+  
         <a href="https://stackoverflow.com/users/16756150/alma-braun" target="_blank" rel="noreferrer">
-          
+          <StackOverflow />
         </a>
       </div>
     </footer>
+  
   );
-}
+  }
 
-export default Footer;
+
