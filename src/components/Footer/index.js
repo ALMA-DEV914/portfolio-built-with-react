c8feeb88cad87d6e57c7367416d8291e0261a32c
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-30 01:03:54 -0800

    update link in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,11 +6,11 @@ export default function Footer(){
   return (
       <footer className="footer">
       <div>
-        <a href="https://github.com/alma-dev914" target="_blank" rel="noreferrer"> <Linkedin />
+        <a href="https://github.com/alma-dev914" target="_blank" rel="noreferrer"> <Github />
         </a>
 
         <a href="https://www.linkedin.com/in/alma-braun-1b11731aa/" target="_blank" rel="noreferrer">
-          <Github />
+        <Linkedin />
         </a>
   
         <a href="https://stackoverflow.com/users/16756150/alma-braun" target="_blank" rel="noreferrer">
