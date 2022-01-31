2849f6b7e87196a8a8b7b09d29289c3b897b5de5
ALMA_DEV914 <aungonalna58@gamil.com>
2022-01-30 17:04:55 -0800

    enhance layout of projects

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ function About(){
             <img src={coverImage} className="my-2" id='me' alt="me" style={{width: '250px'}} />
         </div>
        <div className='description'>
-           <h1>FULL STACK WEB DEVELOPER</h1>
+           <h1 className='desc' >FULL STACK WEB DEVELOPER</h1>
           <p>I'm higly motivated and creative with passion for great user experience. I have completed Full Stack Web Development from UC Berkeley Bootcamp.</p>
           <p>My mission is to ensure the best relationship with clients, both during and after their goals have been met.</p>
           <p>I consistently work on development process to provide an informative, user-friendly and effective strategy to provide companies with the message or goal they are hoping to accomplish. This development process is tailored to meet the needs of small, medium and large size businesses and enterprises.</p>
--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -11,7 +11,10 @@ function ProjectList(props) {
       <ul className="flex-row">
         {projects.map((project) => (
           <li key={project.title}>
-            <h3>{project.title}</h3>
+            <h3>{project.title}</h3><br></br>
+            <img alt="projects" className="img-thumbnail mx-2"
+              src={require(`../../assets/${project.image}`)}
+            ></img>
             <div className="flex-row" id="icons">
             <p className="btn"><a href={project.deployment} target="_blank" rel="noreferrer">
               {" "}<span>View the App</span>
@@ -24,9 +27,6 @@ function ProjectList(props) {
               <img src={githubImage} className='my-2' style={{width: '40px'}} alt='cover'/>
             </a></p>
             </div>
-             <img alt="projects" className="img-thumbnail mx-2"
-              src={require(`../../assets/${project.image}`)}
-            ></img>
           </li>
         ))}
       </ul>
