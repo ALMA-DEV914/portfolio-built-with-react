56bd52147fcfde8b292b4d30dbde7579065d022e
ALMA_DEV914 <aungonalna58@gamil.com>
2022-05-10 14:48:16 -0700

    update conatct form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
   const [errorMessage, setErrorMessage] = useState("");
-
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const { name, email, message } = formState;
+  const firstNameRef = useRef(null)
+  const lastNameRef =  useRef(null)
+  const emailRef = useRef(null)
+  const messageRef =  useRef(null);
 
   function handleChange(e) {
     if (e.target.name === "email") {
@@ -28,23 +25,21 @@ function ContactForm() {
         setErrorMessage("");
       }
     }
-    // setFormState({ ...formState, name: e.target.value });
-    // setFormState({ ...formState, [e.target.name]: e.target.value });
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
+}
+  const handleSubmit = (event) => {
+    event.preventDefault()
+ const data = {
+          firstName: firstNameRef.current.value,
+          lastName: lastNameRef.current.value,
+          email: emailRef.current.value,
+          message: messageRef.current.value
+          }
+    alert("Thank you for contacting us! \n" + "We received your data 😎" + JSON.stringify(data))
+}
 
-    // console.log("errorMessage", errorMessage);
-    // console.log(e.target.name);
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
-  }
-  //   console.log(formState);
-  // JSX
   return (
+    <>
     <main>
     <section id="contact">
       <h1 data-testid="h1tag">Contact me</h1>
@@ -55,32 +50,50 @@ function ContactForm() {
       </div>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            defaultValue={name}
-            onBlur={handleChange}
-            name="name"
-          />
+        <label htmlFor="firstName">First name</label>
+      <input
+           type="text"
+           name="firstName"
+           className="firstName"
+           tabIndex="1"
+           ref={firstNameRef}
+           onBlur={handleChange}
+      />
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            defaultValue={email}
-            onBlur={handleChange}
-            name="email"
-          />
+        <label htmlFor="lastName">Last name</label>
+      <input
+           type="text"
+           name="lastName"
+           className="lastName"
+           tabIndex="2"
+           ref={lastNameRef}
+           onBlur={handleChange}
+      />
         </div>
         <div>
-          <label htmlFor="message">Message:</label><br></br>
-          <textarea
-            name="message"
-            defaultValue={message}
-            onBlur={handleChange}
-            rows="5"
-          />
-        </div>
+        <label htmlFor="email">Email</label><br></br>
+     <input
+     type="email"
+     name="email"
+     id="email"
+     className="message"
+     placeholder="example@gmail.com"
+     tabIndex="3"
+     ref={emailRef}
+     onBlur={handleChange}
+    />
+    </div>
+    <div>
+    <label htmlFor="message">Message</label>
+<textarea
+  placeholder="Start typing..."
+  className="message"
+  name="message"
+  ref={messageRef}
+  onBlur={handleChange}
+/>
+    </div>
         {errorMessage && (
           <div>
             <p className="error-text">{errorMessage}</p>
@@ -91,6 +104,8 @@ function ContactForm() {
       </form>
       </section>
       </main>
+
+      </>
   );
         }
 export default ContactForm;
